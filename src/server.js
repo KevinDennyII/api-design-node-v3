b8bac317e8b107b58aecbd28b7172c22331f643b
kevindennyii <kevin.dennyii@gmail.com>
2020-09-22 02:14:58 -0500

    completed lesson with comments

--- src/server.js
+++ src/server.js
@@ -12,4 +12,27 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const log = (req, res, next) => {
+  console.log('logging')
+  // next essentially is our indicator for middleware and says execute
+  // the next order of middleware
+  next()
+}
+/*
+many different ways to configure middleware
+*/
+
+app.get('/', [log, log, log], (req, res) => {
+  res.send({ message: ' Hello' })
+})
+
+app.post(`/`, (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+
+export const start = () => {
+  app.listen(4000, () => {
+    console.log('server is on 4000')
+  })
+}
