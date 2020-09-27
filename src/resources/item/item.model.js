87f280c543dc6e2bcaea3f6fa0921c5a84a6b19c
kevindennyii <kevin.dennyii@gmail.com>
2020-09-27 16:56:12 -0500

    completed lesson 3 with bonus creating compound index

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,36 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['active', 'complete', 'pastdue'],
+      default: 'active'
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'user',
+      required: true
+    },
+    list: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'list',
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+// compund index to ensure all tasks in a list have unique names
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
+
 export const Item = mongoose.model('item', itemSchema)
